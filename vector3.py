4c545da34f35f79e00dfe7be4facf452812d848a
cmd410 <stresspassing@gmail.com>
2020-06-02 18:58:30 +0300

    Update vector3

--- vector3.py
+++ vector3.py
@@ -1,11 +1,7 @@
-from math import sqrt, floor, ceil, trunc, cos, sin, pi
+from math import *
+from itertools import product
 
 class Vec3:
-    def __eq__(self, other):
-        if isinstance(other, Vec3):
-            return all([self.x == other.x, self.y == other.y, self.z == other.z])
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
-    
     def distance_to(self, other):
         return sqrt(sum([pow(self.x - other.x, 2),
                          pow(self.y - other.y, 2),
@@ -14,9 +10,16 @@ class Vec3:
     def as_dict(self):
         return {'x': self.x, 'y': self.y, 'z': self.z}
 
+    def as_tuple(self):
+        return (self.x, self.y, self.z)
+
     @staticmethod
     def from_tuple(t):
         return Vec3(t[0], t[1], t[2])
+
+    @staticmethod
+    def from_dict(d):
+        return Vec3(d['x'], d['y'], d['z'])
 
     @staticmethod
     def from_yaw(yaw: float):
@@ -30,13 +33,24 @@ class Vec3:
                          pow(self.y, 2),
                          pow(self.z, 2)]))
 
-    def __hash__(self):
-        return hash((self.x, self.y, self.z))
+    def increment_neigbours(self):
+        for i in product([0, 1, -1], repeat=3):
+            vec = Vec3.from_tuple(i) + self
+            if vec == self:
+                continue
+            yield vec
 
     def dot(self, other):
         return sum([self.x * other.x,
                     self.x * other.x,
                     self.x * other.x])
+
+    def cross(self, other):
+        if isinstance(other, Vec3):
+            return Vec3(self.y * other.z - self.z * other.y,
+                        self.z * other.x - self.x * other.z,
+                        self.x * other.y - self.y * other.x)
+        return NotImplemented
 
     def unit(self):
         l = self.length
@@ -46,7 +60,15 @@ class Vec3:
         self.x = x
         self.y = y
         self.z = z
-    
+
+    def __eq__(self, other):
+        if isinstance(other, Vec3):
+            return all([self.x == other.x, self.y == other.y, self.z == other.z])
+        return NotImplemented
+
+    def __hash__(self):
+        return hash((self.x, self.y, self.z))
+
     def __str__(self):
         return f'Vec3({self.x}, {self.y}, {self.z})'
 
@@ -57,59 +79,98 @@ class Vec3:
         return iterator()
 
     def __getitem__(self, key):
+        if isinstance(key, int): return (self.x, self.y, self.z)[key]
         return getattr(self, key)
 
     def __add__(self, other):
         if isinstance(other, Vec3):
             return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __sub__(self, other):
         if isinstance(other, Vec3):
             return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __mul__(self, other):
         if isinstance(other, Vec3):
             return Vec3(self.x * other.x, self.y * other.y, self.z * other.z)
         if isinstance(other, int) or isinstance(other, float):
             return Vec3(self.x * other, self.y * other, self.z * other)
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __truediv__(self, other):
         if isinstance(other, Vec3):
             return Vec3(self.x / other.x, self.y / other.y, self.z / other.z)
         if isinstance(other, int) or isinstance(other, float):
             return Vec3(self.x / other, self.y / other, self.z / other)
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __floordiv__(self, other):
         if isinstance(other, Vec3):
             return Vec3(self.x // other.x, self.y // other.y, self.z // other.z)
         if isinstance(other, int) or isinstance(other, float):
             return Vec3(self.x // other, self.y // other, self.z // other)
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
     
     def __pow__(self, other):
         if isinstance(other, Vec3):
             return Vec3(self.x ** other.x, self.y ** other.y, self.z ** other.z)
         if isinstance(other, int) or isinstance(other, float):
             return Vec3(self.x ** other, self.y ** other, self.z ** other)
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
+
+    def __radd__(self, other):
+        if isinstance(other, Vec3):
+            return other + self
+        return NotImplemented
+
+    def __rsub__(self, other):
+        if isinstance(other, Vec3):
+            return other - self
+        return NotImplemented
+
+    def __rmul__(self, other):
+        if isinstance(other, Vec3):
+            return Vec3(self.x * other.x, self.y * other.y, self.z * other.z)
+        if isinstance(other, int) or isinstance(other, float):
+            return Vec3(self.x * other, self.y * other, self.z * other)
+        return NotImplemented
+
+    def __rtruediv__(self, other):
+        if isinstance(other, Vec3):
+            return other / self
+        if isinstance(other, int) or isinstance(other, float):
+            return Vec3(other / self.x, other / self.y, other / self.z)
+        return NotImplemented
+
+    def __rfloordiv__(self, other):
+        if isinstance(other, Vec3):
+            return other // self
+        if isinstance(other, int) or isinstance(other, float):
+            return Vec3(other // self.x, other // self.y, other // self.z)
+        return NotImplemented
+    
+    def __rpow__(self, other):
+        if isinstance(other, Vec3):
+            return other ** self
+        if isinstance(other, int) or isinstance(other, float):
+            return Vec3(other ** self.x, other ** self.y, other ** self.z)
+        return NotImplemented
 
     def __iadd__(self, other):
         if isinstance(other, Vec3): 
             self.x, self,y, self,z = (self.x + other.x, 
                                       self.y + other.y, 
                                       self.z + other.z)
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __isub__(self, other):
         if isinstance(other, Vec3): 
             self.x, self,y, self,z = (self.x - other.x, 
                                       self.y - other.y, 
                                       self.z - other.z)
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __imul__(self, other):
         if isinstance(other, Vec3): 
@@ -120,7 +181,7 @@ class Vec3:
             self.x, self,y, self,z = (self.x * other, 
                                       self.y * other, 
                                       self.z * other)                                                    
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __itruediv__(self, other):
         if isinstance(other, Vec3): 
@@ -131,7 +192,7 @@ class Vec3:
             self.x, self,y, self,z = (self.x / other, 
                                       self.y / other, 
                                       self.z / other)                                                    
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __ifloordiv__(self, other):
         if isinstance(other, Vec3): 
@@ -142,7 +203,7 @@ class Vec3:
             self.x, self,y, self,z = (self.x // other, 
                                       self.y // other, 
                                       self.z // other)                                                    
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __ipow__(self, other):
         if isinstance(other, Vec3): 
@@ -153,13 +214,10 @@ class Vec3:
             self.x, self,y, self,z = (self.x ** other, 
                                       self.y ** other, 
                                       self.z ** other)                                                    
-        raise NotImplementedError(f'Unsupported type for operation: {type(other)}')
+        return NotImplemented
 
     def __abs__(self):
         return Vec3(abs(self.x), abs(self.y), abs(self.z))
-
-    def __invert__(self):
-        return Vec3(~self.x, ~self.y, ~self.z)
 
     def __neg__(self):
         return Vec3(-self.x, -self.y, -self.z)
